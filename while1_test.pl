b7a65a20a7d8511c9c5e3228ecddbcf6277a383e
shane adams <adamsch1@yahoo.com>
2013-10-20 05:08:10 -0700

    while test case

---
+++ while1_test.pl
@@ -0,0 +1,16 @@
+var x;
+
+
+procedure foo()
+begin
+end;
+
+begin
+  x := 1;
+  while x < 100 do begin x := x + 1;  end
+  !x;
+  while x > 0  do begin x := x - 1;  end
+  !x;
+  while x < 1  do begin x := x - 1;  end
+  !x;
+end.
